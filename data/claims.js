aaded3a2697a90207fd7b58492540d1db1fd771c
Kulanthaivel <539617@cognizant.com>
2020-08-14 03:51:37 +0530

    changed

--- data/claims.js
+++ data/claims.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 
-// const baseUrl =  process.env.PHARMACIST_URL;
-const baseUrl = 'https://patient-arun205.sandbox-ocp43-one-176292-be5b1ee812fa4041cc73b6bbf969fc88-0000.eu-gb.containers.appdomain.cloud';
+const baseUrl =  process.env.PATIENT_URL;
 
 const getClaims = () => {
   return apiGetCall();
